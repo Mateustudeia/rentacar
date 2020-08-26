2f2276da4910249ee90147309a62c0993f49bc52
okellyson <=>
2020-08-26 00:20:41 -0300

    REPOSITORY - Início da conexão com o banco de dados

---
+++ Repository/Helper/FluentNHibernateHelper.cs
@@ -0,0 +1,49 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Helper
+{
+    public static class FluentNHibernateHelper
+    {
+        private static ISessionFactory session;
+
+        public static ISessionFactory CriarSession()
+        {
+            if (session != null)
+                return session;
+
+            ISessionFactory sessionFactory = Fluently.Configure()
+
+                .Database(PostgreSQLConfiguration.PostgreSQL82
+
+                    .ConnectionString(c =>
+                        c.Host("localhost")
+                            .Port(5432)
+                            .Database("rentacar")
+                            .Username("postgres")
+                            .Password("postgres")
+                    ).DefaultSchema("public"))
+                //.Mappings(m =>
+                   // m.FluentMappings
+                        //.AddFromAssemblyOf<ENTITY>()
+
+                .ExposeConfiguration(cfg => new SchemaExport(cfg)
+                    .Create(false, false))
+                .BuildSessionFactory();
+
+            return sessionFactory;
+        }
+
+        public static ISession AbrirSession()
+        {
+            return CriarSession().OpenSession();
+        }
+    }
+}
