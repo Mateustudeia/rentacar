95f1b33f307d1198d8d67c56c05863e6f068d71a
João Victor Teófilo Salgado <joao.teofilo@geotech4web.com.br>
2020-08-25 22:32:27 -0300

    Criação de homepage e remoção de arquivos desnecessários.

--- rentacar/App_Start/RouteConfig.cs
+++ rentacar/App_Start/RouteConfig.cs
@@ -10,9 +10,7 @@ namespace rentacar
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            var settings = new FriendlyUrlSettings();
-            settings.AutoRedirectMode = RedirectMode.Permanent;
-            routes.EnableFriendlyUrls(settings);
+            
         }
     }
 }
