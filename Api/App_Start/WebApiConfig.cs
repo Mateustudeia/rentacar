6007946f57b73aaf25f49b75b59cc0f8f3736a33
okellyson <=>
2020-08-27 23:50:36 -0300

    Formatando rotas e respostas em json

--- Api/App_Start/WebApiConfig.cs
+++ Api/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace Api
@@ -14,9 +12,12 @@ namespace Api
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
