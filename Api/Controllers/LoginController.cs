3521e5d7a62f765fee2ea36e3fab502bc6891b38
João Victor Teófilo Salgado <joao.teofilo@geotech4web.com.br>
2020-08-31 08:41:28 -0300

    Correção de rota de login.

--- Api/Controllers/LoginController.cs
+++ Api/Controllers/LoginController.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
 using Repository.Entities;
 using Repository.Repositories;
 
 namespace Api.Controllers
 {
-    public class LoginController : Controller
+    public class Usuario
+    {
+        public string login { get; set; }
+        public string senha { get; set; }
+    }
+    public class LoginController : ApiController
     {
         [HttpPost]
-        public bool Logar(string login, string senha)
+        public bool Logar([FromBody] Usuario usuario)
         {
             UsuarioRepositorio repositorio = new UsuarioRepositorio();
 
-            return repositorio.Login(login, senha);
+            return repositorio.Login(usuario.login, usuario.senha);
         }
     }
 }
